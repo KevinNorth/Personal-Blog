10c13b6b3496396c6e2b6cfd03ed91864db14402
Kevin North <thepondermatic@gmail.com>
2023-10-16 22:39:55 -0500

    chore: install initial npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
-const path    = require("path")
+const path = require("path")
 const webpack = require("webpack")
+
+// Extracts CSS into .css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: [
+      './app/javascript/application.js',
+      './app/assets/stylesheets/application.scss',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx|)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.css'],
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
   ]
 }
