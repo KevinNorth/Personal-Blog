d896159378b039029776ef24a748e92defca03f3
Kevin North <thepondermatic@gmail.com>
2023-10-22 23:20:38 -0500

    test(QueryType): test user_by_login query

--- spec/graphql/queries/user_by_login_spec.rb
+++ spec/graphql/queries/user_by_login_spec.rb
@@ -3,5 +3,74 @@
 require 'rails_helper'
 
 RSpec.describe 'user_by_login', type: :request do
-  pending 'needs tests'
+  describe 'when given login argument for a user that exists' do
+    let(:login) { 'test' }
+    let!(:user) { create(:user, login:) }
+    let(:category) { create(:category) }
+    let!(:posts_by_user) { create_list(:post, 3, author: user, category:) }
+
+    it 'responds with the corresponding user' do
+      query = <<~GQL
+        query {
+          userByLogin(login: "#{login}") {
+            admin
+            createdAt
+            id
+            login
+            name
+            posts {
+              id
+            }
+            updatedAt
+          }
+        }
+      GQL
+
+      post graphql_path, params: { query: }
+      json = JSON.parse(response.body)
+      result = json['data']['userByLogin']
+
+      expect(result).to include(
+        'admin' => user.admin,
+        'id' => user.id.to_s,
+        'login' => login,
+        'name' => user.name
+      )
+
+      expect(DateTime.parse(result['createdAt'])).to eq(strip_milliseconds(user.created_at))
+      expect(DateTime.parse(result['updatedAt'])).to eq(strip_milliseconds(user.updated_at))
+
+      expected_post_ids = posts_by_user.pluck(:id).map(&:to_s)
+      actual_post_ids = result['posts'].pluck('id')
+      expect(actual_post_ids).to match_array(expected_post_ids)
+    end
+  end
+
+  describe 'when given login argument that does not match a user' do
+    let(:invalid_login) { 'This is not in the database' }
+
+    it 'responds with null' do
+      query = <<~GQL
+        query {
+          userByLogin(login: #{invalid_login}) {
+            admin
+            created_at
+            id
+            login
+            name
+            posts {
+              id
+            }
+            updated_at
+          }
+        }
+      GQL
+
+      post graphql_path, params: { query: }
+      json = JSON.parse(response.body)
+      result = json['data']
+
+      expect(result).to be_nil
+    end
+  end
 end
