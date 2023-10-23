d6e5fe5db2d210f1014f1e3d299d195cc2430156
Kevin North <thepondermatic@gmail.com>
2023-10-22 23:48:06 -0500

    test(QueryType): test post_by_id query

--- spec/graphql/queries/post_by_id_spec.rb
+++ spec/graphql/queries/post_by_id_spec.rb
@@ -3,5 +3,136 @@
 require 'rails_helper'
 
 RSpec.describe 'post_by_id', type: :request do
-  pending 'needs tests'
+  shared_examples 'responds with post' do
+    it 'responds with the corresponding post' do
+      query = <<~GQL
+        query {
+          postById(id: #{db_post.id}, includeUnpublished: #{include_unpublished}) {
+            author {
+              id
+            }
+            category {
+              id
+            }
+            createdAt
+            id
+            markdown
+            order
+            published
+            slug
+            subtitle
+            summary
+            title
+            updatedAt
+          }
+        }
+      GQL
+
+      post graphql_path, params: { query: }
+      json = JSON.parse(response.body)
+      result = json['data']['postById']
+
+      expect(result).to include(
+        'id' => db_post.id.to_s,
+        'markdown' => db_post.markdown,
+        'order' => db_post.order,
+        'published' => db_post.published,
+        'slug' => db_post.slug,
+        'subtitle' => db_post.subtitle,
+        'summary' => db_post.summary,
+        'title' => db_post.title
+      )
+
+      expect(DateTime.parse(result['createdAt'])).to eq(strip_milliseconds(db_post.created_at))
+      expect(DateTime.parse(result['updatedAt'])).to eq(strip_milliseconds(db_post.updated_at))
+
+      expect(result['author']['id']).to eq(db_post.author.id.to_s)
+      expect(result['category']['id']).to eq(db_post.category.id.to_s)
+    end
+  end
+
+  shared_examples 'responds with null' do
+    it 'responds with null' do
+      query = <<~GQL
+        query {
+          postById(id: #{id}, includeUnpublished: #{include_unpublished}) {
+            admin
+            created_at
+            id
+            login
+            name
+            posts {
+              id
+            }
+            updated_at
+          }
+        }
+      GQL
+
+      post graphql_path, params: { query: }
+      json = JSON.parse(response.body)
+      result = json['data']
+
+      expect(result).to be_nil
+    end
+  end
+
+  describe 'when given id argument for a post that exists' do
+    describe 'when include_unpublished is false' do
+      describe 'when the post is published' do
+        let(:author) { create(:user) }
+        let(:category) { create(:category) }
+        let!(:db_post) { create(:post, author:, category:, published: true) }
+        let(:include_unpublished) { false }
+
+        include_examples 'responds with post'
+      end
+
+      describe 'when the post is not published' do
+        let(:author) { create(:user) }
+        let(:category) { create(:category) }
+        let!(:db_post) { create(:post, author:, category:, published: false) }
+        let(:include_unpublished) { false }
+        let(:id) { db_post.id }
+
+        include_examples 'responds with null'
+      end
+    end
+
+    describe 'when include_unpublished is true' do
+      describe 'when the post is published' do
+        let(:author) { create(:user) }
+        let(:category) { create(:category) }
+        let!(:db_post) { create(:post, author:, category:, published: true) }
+        let(:include_unpublished) { true }
+
+        include_examples 'responds with post'
+      end
+
+      describe 'when the post is not published' do
+        let(:author) { create(:user) }
+        let(:category) { create(:category) }
+        let!(:db_post) { create(:post, author:, category:, published: false) }
+        let(:include_unpublished) { true }
+
+        include_examples 'responds with post'
+      end
+    end
+  end
+
+  describe 'when given id argument that does not match a post' do
+    let(:id) { Post.count == 0 ? 1 : Post.maximum(id) }
+
+    describe 'when include_unpublished is false' do
+      let(:include_unpublished) { false }
+
+      include_examples 'responds with null'
+    end
+
+    describe 'when include_unpublished is true' do
+      let(:include_unpublished) { true }
+
+      include_examples 'responds with null'
+    end
+  end
 end
