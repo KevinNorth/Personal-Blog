f375db49330b4080921678e414350c4d5a0d5e45
Kevin <kevinjamesnorth@outlook.com>
2023-11-08 08:20:14 +0000

    fix(render-build.sh): attempt to fix problems with deploy

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,13 @@
 # exit on error
 set -o errexit
 
+npm install -g corepack
+corepack enable
+corepack prepare pnpm@latest --activate
+npm install -g pnpm
+
+pnpm install
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
