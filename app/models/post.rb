39e8ed035332ec3190a32784262d3ad11d6e1e2b
Kevin North <kevinjamesnorth@outlook.com>
2024-07-04 23:14:27 +0000

    fix: fix issues with running migration forwards

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,34 +4,41 @@
 #
 # Table name: posts
 #
-#  id           :bigint           not null, primary key
-#  header_image :binary
-#  markdown     :text
-#  order        :integer
-#  published    :boolean          default(FALSE), not null
-#  slug         :string           not null
-#  subtitle     :string
-#  summary      :string
-#  title        :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  author_id    :bigint           not null
-#  category_id  :bigint           not null
+#  id                             :bigint           not null, primary key
+#  header_image                   :binary
+#  markdown                       :text
+#  name                           :string           default(""), not null
+#  order                          :integer
+#  previous_id_as_category        :integer
+#  previous_order_as_category     :integer          default(1)
+#  previous_parent_id_as_category :integer
+#  published                      :boolean          default(FALSE), not null
+#  slug                           :string           not null
+#  subtitle                       :string
+#  summary                        :string
+#  title                          :string
+#  was_category                   :boolean          default(FALSE), not null
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#  author_id                      :bigint           not null
+#  category_id                    :bigint
+#  parent_id                      :bigint
 #
 # Indexes
 #
-#  index_posts_on_author_id    (author_id)
-#  index_posts_on_category_id  (category_id)
+#  index_posts_on_author_id  (author_id)
+#  index_posts_on_parent_id  (parent_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (author_id => users.id)
-#  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (parent_id => posts.id)
 #
 class Post < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :children, class_name: 'Post', foreign_key: :parent_id, inverse_of: :parent, dependent: :nullify
   belongs_to :parent, class_name: 'Post', inverse_of: :children, optional: true
+  belongs_to :category, optional: true
   belongs_to :author, class_name: 'User', inverse_of: :posts
 
   # These columns are exclusively used to keep track of metadata needed to make
@@ -44,5 +51,5 @@ class Post < ApplicationRecord
   #   previous_order_as_category
   # ]
 
-  validates :order, uniqueness: { scope: :category_id }
+  validates :order, uniqueness: { scope: :parent_id }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,29 +4,35 @@
 #
 # Table name: posts
 #
-#  id           :bigint           not null, primary key
-#  header_image :binary
-#  markdown     :text
-#  order        :integer
-#  published    :boolean          default(FALSE), not null
-#  slug         :string           not null
-#  subtitle     :string
-#  summary      :string
-#  title        :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  author_id    :bigint           not null
-#  category_id  :bigint           not null
+#  id                             :bigint           not null, primary key
+#  header_image                   :binary
+#  markdown                       :text
+#  name                           :string           default(""), not null
+#  order                          :integer
+#  previous_id_as_category        :integer
+#  previous_order_as_category     :integer          default(1)
+#  previous_parent_id_as_category :integer
+#  published                      :boolean          default(FALSE), not null
+#  slug                           :string           not null
+#  subtitle                       :string
+#  summary                        :string
+#  title                          :string
+#  was_category                   :boolean          default(FALSE), not null
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#  author_id                      :bigint           not null
+#  category_id                    :bigint
+#  parent_id                      :bigint
 #
 # Indexes
 #
-#  index_posts_on_author_id    (author_id)
-#  index_posts_on_category_id  (category_id)
+#  index_posts_on_author_id  (author_id)
+#  index_posts_on_parent_id  (parent_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (author_id => users.id)
-#  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (parent_id => posts.id)
 #
 require 'rails_helper'
 
